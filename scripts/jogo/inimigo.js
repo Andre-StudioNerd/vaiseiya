d2c06aa3b68fca468d130ce6d2a3b2c917602995
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-26 15:08:39 -0300

    Update inimigo.js

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,34 +1,4 @@
-
-class Inimigo extends Animacao {
-  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade, delay) {
-    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite)
-
-    this.velocidade = velocidade
-    this.delay = delay
-    this.x = width + this.delay
-  }
-
-  move() {
-    this.x = this.x - this.velocidade;
-    if (this.x < -this.largura - this.delay) {
-      this.x = width
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Inimigo_antigo extends Animacao{
+class Inimigo extends Animacao{
   constructor(imagem, matriz, x, y, largura, altura, lSprite, aSprite){
     super(imagem, matriz, x, y, largura, altura, lSprite, aSprite);
 
